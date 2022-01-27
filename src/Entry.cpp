2409ef23041a8c558dcd1aab8a1fb464b348b216
Cian Jinks <cjinks99@gmail.com>
2022-01-27 10:38:38 +0000

    Reverted strcpy_s usage Building on linux failed when using strcpy_s. Using std::strcpy instead regardless of windows warnings.

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <map>
 #include <algorithm>
-#include <string.h>
+#include <cstring>
 
 #include "imgui.h"
 #include "imgui_internal.h"
@@ -50,11 +50,11 @@ float renderDistance = 10000.0f;
 bool debug = false;
 bool mipmap = true;
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
 }
 
-void poll_input(GLFWwindow* window)
+void poll_input(GLFWwindow *window)
 {
 	if (glfwGetInputMode(window, GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
 	{
@@ -89,7 +89,7 @@ void poll_input(GLFWwindow* window)
 	}
 }
 
-void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+void cursor_position_callback(GLFWwindow *window, double xpos, double ypos)
 {
 	if (glfwGetInputMode(window, GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
 	{
@@ -127,7 +127,7 @@ void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 	cameraFront = glm::normalize(direction);
 }
 
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
 {
 	if (ImGui::GetIO().WantCaptureMouse)
 	{
@@ -149,20 +149,20 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 }
 
 void GLAPIENTRY MessageCallback(GLenum source,
-	GLenum type,
-	GLuint id,
-	GLenum severity,
-	GLsizei length,
-	const GLchar* message,
-	const void* userParam)
+								GLenum type,
+								GLuint id,
+								GLenum severity,
+								GLsizei length,
+								const GLchar *message,
+								const void *userParam)
 {
 	fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
-		(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
-		type, severity, message);
+			(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+			type, severity, message);
 }
 
-static Ciri::SceneNode* selected_node = nullptr;
-void SceneUI(Ciri::SceneNode* root, int ptr_id)
+static Ciri::SceneNode *selected_node = nullptr;
+void SceneUI(Ciri::SceneNode *root, int ptr_id)
 {
 	ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_FramePadding;
 
@@ -181,7 +181,7 @@ void SceneUI(Ciri::SceneNode* root, int ptr_id)
 
 	ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(2.0f, 2.0f));
 	// TODO: Blank root name is buggy and icon handling is rough
-	bool node_open = ImGui::TreeNodeEx((void*)(intptr_t)ptr_id, node_flags, "%s %s", root->NodeMesh ? ICON_FK_CUBE : ICON_FK_SQUARE_O, root->Name.c_str());
+	bool node_open = ImGui::TreeNodeEx((void *)(intptr_t)ptr_id, node_flags, "%s %s", root->NodeMesh ? ICON_FK_CUBE : ICON_FK_SQUARE_O, root->Name.c_str());
 
 	// Update selected node on click but not expand toggle click
 	if (ImGui::IsItemClicked() && !ImGui::IsItemToggledOpen())
@@ -191,7 +191,7 @@ void SceneUI(Ciri::SceneNode* root, int ptr_id)
 
 	if (node_open && !leaf)
 	{
-		for (Ciri::SceneNode* node : root->m_Children)
+		for (Ciri::SceneNode *node : root->m_Children)
 		{
 			SceneUI(node, ptr_id++);
 		}
@@ -201,9 +201,9 @@ void SceneUI(Ciri::SceneNode* root, int ptr_id)
 	ImGui::PopStyleVar();
 }
 
-void RenderScene(Ciri::SceneNode* root, Ciri::ShaderType& selected, Ciri::ShaderLibrary* library, glm::mat4& proj, glm::mat4& view, glm::mat4 model)
+void RenderScene(Ciri::SceneNode *root, Ciri::ShaderType &selected, Ciri::ShaderLibrary *library, glm::mat4 &proj, glm::mat4 &view, glm::mat4 model)
 {
-	for (Ciri::SceneNode* node : root->m_Children)
+	for (Ciri::SceneNode *node : root->m_Children)
 	{
 		glm::mat4 accumulateModel = model;
 		accumulateModel = glm::translate(accumulateModel, node->Position);
@@ -217,7 +217,7 @@ void RenderScene(Ciri::SceneNode* root, Ciri::ShaderType& selected, Ciri::Shader
 			library->BindShader(selected);
 			library->SetMat4f("u_MVP", glm::value_ptr(mvp));
 
-			Ciri::Material* material = node->NodeMaterial;
+			Ciri::Material *material = node->NodeMaterial;
 			library->SetInt1i("u_BaseColorTexture", 0);
 			library->SetVec3f("u_BaseColor", material->baseColor);
 			glActiveTexture(GL_TEXTURE0);
@@ -238,7 +238,7 @@ void RenderScene(Ciri::SceneNode* root, Ciri::ShaderType& selected, Ciri::Shader
 			glActiveTexture(GL_TEXTURE5);
 			glBindTexture(GL_TEXTURE_2D, material->occlusionTextureID);
 
-			Ciri::Mesh* mesh = node->NodeMesh;
+			Ciri::Mesh *mesh = node->NodeMesh;
 			glBindVertexArray(mesh->m_VAO);
 
 			glDrawArrays(GL_TRIANGLES, 0, 3 * mesh->TriCount);
@@ -252,13 +252,13 @@ void RenderScene(Ciri::SceneNode* root, Ciri::ShaderType& selected, Ciri::Shader
 	}
 }
 
-void MaterialLibraryUI(Ciri::MaterialLibrary& library, Ciri::Material*& selected_material, ImGuiWindowFlags& flags)
+void MaterialLibraryUI(Ciri::MaterialLibrary &library, Ciri::Material *&selected_material, ImGuiWindowFlags &flags)
 {
 	ImGui::Begin("Materials", NULL, flags);
-	for (auto& pair : library.GetMaterials())
+	for (auto &pair : library.GetMaterials())
 	{
 		std::string name = pair.first;
-		Ciri::Material* material = pair.second;
+		Ciri::Material *material = pair.second;
 		std::string text = ICON_FK_CODEPEN + std::string(" ") + name;
 		if (ImGui::Selectable(text.c_str(), selected_material == material))
 		{
@@ -268,10 +268,10 @@ void MaterialLibraryUI(Ciri::MaterialLibrary& library, Ciri::Material*& selected
 	ImGui::End();
 }
 
-void CreateTextureCombo(const char* name, uint32_t& material_tex_id, Ciri::Scene* scene)
+void CreateTextureCombo(const char *name, uint32_t &material_tex_id, Ciri::Scene *scene)
 {
 	std::string texture_name = "None";
-	for (auto& pair : scene->MatLib.GetTextures())
+	for (auto &pair : scene->MatLib.GetTextures())
 	{
 		if (material_tex_id == pair.second)
 		{
@@ -281,7 +281,7 @@ void CreateTextureCombo(const char* name, uint32_t& material_tex_id, Ciri::Scene
 
 	if (ImGui::BeginCombo(name, texture_name.c_str(), 0))
 	{
-		for (auto& pair : scene->MatLib.GetTextures())
+		for (auto &pair : scene->MatLib.GetTextures())
 		{
 			std::string tex_name = pair.first;
 			uint32_t texture_id = pair.second;
@@ -295,7 +295,7 @@ void CreateTextureCombo(const char* name, uint32_t& material_tex_id, Ciri::Scene
 	}
 }
 
-void MaterialSettingsUI(Ciri::Material* material, Ciri::Scene* scene, ImGuiWindowFlags& flags)
+void MaterialSettingsUI(Ciri::Material *material, Ciri::Scene *scene, ImGuiWindowFlags &flags)
 {
 	ImGui::Begin("Material Settings", NULL, flags);
 
@@ -326,7 +326,7 @@ void MaterialSettingsUI(Ciri::Material* material, Ciri::Scene* scene, ImGuiWindo
 
 int main()
 {
-	GLFWwindow* window;
+	GLFWwindow *window;
 
 	// GLFW and Glad
 	if (!glfwInit())
@@ -369,15 +369,15 @@ int main()
 	// ImGUI
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
+	ImGuiIO &io = ImGui::GetIO();
 
 	// Merge in icons from Fork Awesome
 	io.Fonts->AddFontDefault();
-	static const ImWchar icons_ranges[] = { ICON_MIN_FK, ICON_MAX_FK, 0 };
+	static const ImWchar icons_ranges[] = {ICON_MIN_FK, ICON_MAX_FK, 0};
 	ImFontConfig icons_config;
 	icons_config.MergeMode = true;
 	icons_config.PixelSnapH = true;
-	icons_config.GlyphOffset = { 0.f, 2.f };
+	icons_config.GlyphOffset = {0.f, 2.f};
 	icons_config.GlyphMinAdvanceX = 20.0f; // necessary?
 	io.Fonts->AddFontFromFileTTF(FONT_ICON_FILE_NAME_FK, 16.0f, &icons_config, icons_ranges);
 
@@ -389,42 +389,42 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 
 	// Shaders
-	Ciri::ShaderLibrary* shaderLibrary = new Ciri::ShaderLibrary();
-	auto& shaders = shaderLibrary->GetShaderList();
-	Ciri::ShaderType selected_shader = Ciri::ShaderType::FLAT_NORMAL;
+	Ciri::ShaderLibrary *shaderLibrary = new Ciri::ShaderLibrary();
+	auto &shaders = shaderLibrary->GetShaderList();
+	Ciri::ShaderType selected_shader = Ciri::ShaderType::ALBEDO_TEXTURE;
 
 	// Scene
-	Ciri::Scene* mainScene = new Ciri::Scene("Main Scene");
-	Ciri::Mesh* cube1 = new Ciri::Cube();
-	Ciri::Mesh* cube2 = new Ciri::Cube();
-	Ciri::Mesh* cube3 = new Ciri::Cube();
+	Ciri::Scene *mainScene = new Ciri::Scene("Main Scene");
+	Ciri::Mesh *cube1 = new Ciri::Cube();
+	Ciri::Mesh *cube2 = new Ciri::Cube();
+	Ciri::Mesh *cube3 = new Ciri::Cube();
 	cube1->Construct();
 	cube2->Construct();
 	cube3->Construct();
-	Ciri::SceneNode* cube1Node = mainScene->AddMesh("cube1", cube1);
-	Ciri::SceneNode* cube2Node = mainScene->AddMesh("cube2", cube2);
-	Ciri::SceneNode* cube3Node = mainScene->AddMesh("cube3", cube3);
+	Ciri::SceneNode *cube1Node = mainScene->AddMesh("cube1", cube1);
+	Ciri::SceneNode *cube2Node = mainScene->AddMesh("cube2", cube2);
+	Ciri::SceneNode *cube3Node = mainScene->AddMesh("cube3", cube3);
 	cube1Node->Position = glm::vec3(-3.0f, 10.0f, 0.0f);
 	cube1Node->Scale = glm::vec3(0.5f);
 	cube2Node->Position = glm::vec3(0.0f, 10.0f, 0.0f);
 	cube2Node->Scale = glm::vec3(0.75f);
 	cube3Node->Position = glm::vec3(3.0f, 10.0f, 0.0f);
 
-	Ciri::SceneNode* sponzaNode = mainScene->LoadModel("sponza", "resources/mesh/sponza/sponza.obj");
-	Ciri::SceneNode* dragonNode = mainScene->LoadModel("dragon", "resources/mesh/dragon/dragon.obj");
+	Ciri::SceneNode *sponzaNode = mainScene->LoadModel("sponza", "resources/mesh/sponza/sponza.obj");
+	Ciri::SceneNode *dragonNode = mainScene->LoadModel("dragon", "resources/mesh/dragon/dragon.obj");
 	sponzaNode->Scale = glm::vec3(0.05f);
 	dragonNode->Position = glm::vec3(10.0f, 10.0f, 0.0f);
 	dragonNode->Scale = glm::vec3(10.0f);
 
 	// UI
-	char* addmesh_name = new char[20]();
+	char *addmesh_name = new char[20]();
 	glm::vec3 addmesh_position = glm::vec3(0.0f);
 	glm::vec3 addmesh_scale = glm::vec3(1.0f);
-	char* addmesh_filepath = new char[100];
-	const char* default_filepath = "resources/mesh/dragon/dragon.obj";
-	strcpy_s(addmesh_filepath, 100, default_filepath);
+	char *addmesh_filepath = new char[100];
+	const char *default_filepath = "resources/mesh/dragon/dragon.obj";
+	std::strcpy(addmesh_filepath, default_filepath);
 
-	Ciri::Material* selected_material = nullptr;
+	Ciri::Material *selected_material = nullptr;
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -455,9 +455,9 @@ int main()
 		ImGui::SliderFloat("Render Distance", &renderDistance, 10.0f, 10000.0f);
 		if (ImGui::BeginCombo("Shading", shaders[selected_shader]->name, 0))
 		{
-			for (auto& pair : shaders)
+			for (auto &pair : shaders)
 			{
-				Ciri::Shader* shader = pair.second;
+				Ciri::Shader *shader = pair.second;
 				const bool is_selected = (selected_shader == shader->type);
 				if (ImGui::Selectable(shader->name, is_selected))
 					selected_shader = shader->type;
@@ -477,7 +477,7 @@ int main()
 
 		if (ImGui::BeginPopupModal("Add Mesh", NULL, ImGuiWindowFlags_AlwaysAutoResize))
 		{
-			const char* mesh_types[] = { "Cube", "Quad", "Load Model.." };
+			const char *mesh_types[] = {"Cube", "Quad", "Load Model.."};
 			static int type = 0;
 			ImGui::Combo("Type", &type, mesh_types, IM_ARRAYSIZE(mesh_types));
 			ImGui::Separator();
@@ -497,25 +497,25 @@ int main()
 				{
 				case 0:
 				{
-					Ciri::Mesh* cube = new Ciri::Cube();
+					Ciri::Mesh *cube = new Ciri::Cube();
 					cube->Construct();
-					Ciri::SceneNode* cubeNode = mainScene->AddMesh(addmesh_name, cube);
+					Ciri::SceneNode *cubeNode = mainScene->AddMesh(addmesh_name, cube);
 					cubeNode->Position = addmesh_position;
 					cubeNode->Scale = addmesh_scale;
 					break;
 				}
 				case 1:
 				{
-					Ciri::Mesh* quad = new Ciri::Quad();
+					Ciri::Mesh *quad = new Ciri::Quad();
 					quad->Construct();
-					Ciri::SceneNode* quadNode = mainScene->AddMesh(addmesh_name, quad);
+					Ciri::SceneNode *quadNode = mainScene->AddMesh(addmesh_name, quad);
 					quadNode->Position = addmesh_position;
 					quadNode->Scale = addmesh_scale;
 					break;
 				}
 				case 2:
 				{
-					Ciri::SceneNode* dragonNode = mainScene->LoadModel(addmesh_name, addmesh_filepath);
+					Ciri::SceneNode *dragonNode = mainScene->LoadModel(addmesh_name, addmesh_filepath);
 					dragonNode->Position = addmesh_position;
 					dragonNode->Scale = addmesh_scale;
 					break;
@@ -527,7 +527,7 @@ int main()
 				std::memset(addmesh_name, 0, 20);
 				addmesh_position = glm::vec3(0.0f);
 				addmesh_scale = glm::vec3(1.0f);
-				strcpy_s(addmesh_filepath, 100, default_filepath);
+				std::strcpy(addmesh_filepath, default_filepath);
 			}
 			ImGui::SetItemDefaultFocus();
 			ImGui::SameLine();
@@ -557,9 +557,9 @@ int main()
 			{
 				if (ImGui::BeginCombo("Material", selected_node->NodeMaterial->name.c_str(), 0))
 				{
-					for (auto& pair : mainScene->MatLib.GetMaterials())
+					for (auto &pair : mainScene->MatLib.GetMaterials())
 					{
-						Ciri::Material* material = pair.second;
+						Ciri::Material *material = pair.second;
 						const bool is_selected = (selected_node->NodeMaterial == material);
 						if (ImGui::Selectable(material->name.c_str(), is_selected))
 							selected_node->NodeMaterial = material;
