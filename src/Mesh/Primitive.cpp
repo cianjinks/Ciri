f9c18f80c0ebdf3487a93f626fadb5a04928e052
Cian Jinks <cjinks99@gmail.com>
2022-11-14 20:18:56 +0000

    Fixed quad orientation to face up

--- src/Mesh/Primitive.cpp
+++ src/Mesh/Primitive.cpp
@@ -146,32 +146,32 @@ namespace Ciri
     {
         // clang-format off
         m_PositionData = {
-            glm::vec3(-1.0f, -1.0f,  1.0f),
-            glm::vec3( 1.0f, -1.0f,  1.0f),
-            glm::vec3( 1.0f,  1.0f,  1.0f),
-            glm::vec3( 1.0f,  1.0f,  1.0f),
-            glm::vec3(-1.0f,  1.0f,  1.0f),
-            glm::vec3(-1.0f, -1.0f,  1.0f)
+            glm::vec3( 1.0f, 0.0f, -1.0f),
+            glm::vec3( 1.0f, 0.0f,  1.0f),
+            glm::vec3(-1.0f, 0.0f,  1.0f),
+            glm::vec3( 1.0f, 0.0f, -1.0f),
+            glm::vec3(-1.0f, 0.0f, -1.0f),
+            glm::vec3(-1.0f, 0.0f,  1.0f)
+
         };
 
         m_NormalData = {
-            glm::vec3( 0.0f,  0.0f,  1.0f),
-            glm::vec3( 0.0f,  0.0f,  1.0f),
-            glm::vec3( 0.0f,  0.0f,  1.0f),
-            glm::vec3( 0.0f,  0.0f,  1.0f),
-            glm::vec3( 0.0f,  0.0f,  1.0f),
-            glm::vec3( 0.0f,  0.0f,  1.0f)
+            glm::vec3( 0.0f,  1.0f,  0.0f),
+            glm::vec3( 0.0f,  1.0f,  0.0f),
+            glm::vec3( 0.0f,  1.0f,  0.0f),
+            glm::vec3( 0.0f,  1.0f,  0.0f),
+            glm::vec3( 0.0f,  1.0f,  0.0f),
+            glm::vec3( 0.0f,  1.0f,  0.0f)
         };
 
         m_TexCoordData = {
-            glm::vec2(0.0f, 0.0f),
-            glm::vec2(1.0f, 0.0f),
             glm::vec2(1.0f, 1.0f),
+            glm::vec2(1.0f, 0.0f),
+            glm::vec2(0.0f, 0.0f),
             glm::vec2(1.0f, 1.0f),
             glm::vec2(0.0f, 1.0f),
             glm::vec2(0.0f, 0.0f)
         };
-
         // clang-format on
     }
 
